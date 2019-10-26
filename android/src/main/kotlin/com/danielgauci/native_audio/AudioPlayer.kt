8d33eeb6f4066b71e4ad905257b9f11d342a3126
Daniel Gauci <daniel.gauci@gmail.com>
2019-10-26 15:06:45 +0200

    Properly nullify progress callback and media player on release

--- android/src/main/kotlin/com/danielgauci/native_audio/AudioPlayer.kt
+++ android/src/main/kotlin/com/danielgauci/native_audio/AudioPlayer.kt
@@ -6,76 +6,35 @@ import android.util.Log
 import java.util.concurrent.TimeUnit
 
 class AudioPlayer(
-        onLoaded: ((duration: Long) -> Unit)? = null,
-        onProgressChanged: ((currentTime: Long) -> Unit)? = null,
-        onCompleted: (() -> Unit)? = null
+        private val onLoaded: ((duration: Long) -> Unit)? = null,
+        private val onProgressChanged: ((currentTime: Long) -> Unit)? = null,
+        private val onCompleted: (() -> Unit)? = null
 ) {
 
-    private val progressCallbackHandler: Handler by lazy { Handler() }
-    private val progressCallback: Runnable by lazy {
-        Runnable {
-            val progress = mediaPlayer.currentPosition.toLong()
-            if (progress != currentProgress) {
-                onProgressChanged?.invoke(progress)
-                currentProgress = progress
-            }
-
-            progressCallbackHandler.postDelayed(progressCallback, TimeUnit.SECONDS.toMillis(1))
-        }
-    }
-
-    private val mediaPlayer: MediaPlayer by lazy {
-        MediaPlayer().apply {
-            setOnPreparedListener {
-                // Start audio once loaded
-                start()
-
-                // Notify callback
-                onLoaded?.invoke(duration.toLong())
-
-                // Setup progress callback
-                progressCallbackHandler.postDelayed(progressCallback, TimeUnit.SECONDS.toMillis(1))
-
-                // Update flags
-                isLoaded = true
-            }
-
-            setOnCompletionListener {
-                // Notify callback
-                onCompleted?.invoke()
-
-                // Clear progress callback
-                progressCallbackHandler.removeCallbacks(progressCallback)
-
-                // Update flags
-                isLoaded = false
-            }
-        }
-    }
-
+    private var mediaPlayer: MediaPlayer? = null
+    private var progressCallbackHandler: Handler? = null
+    private var progressCallback: Runnable? = null
     private var currentProgress = 0L
     private var isLoaded = false
 
     fun play(url: String) {
-        if (mediaPlayer.isPlaying) stop()
+        if (mediaPlayer == null) initMediaPlayer()
+
+        if (mediaPlayer?.isPlaying == true) stop()
 
         loadAudio(url) { resume() }
     }
 
     fun resume() {
-        mediaPlayer.apply {
-            if (isLoaded && !isPlaying) mediaPlayer.start()
-        }
+        mediaPlayer?.apply { if (isLoaded && !isPlaying) start() }
     }
 
     fun pause() {
-        mediaPlayer.apply {
-            if (isPlaying) mediaPlayer.pause()
-        }
+        mediaPlayer?.apply { if (isPlaying) pause() }
     }
 
     fun stop(release: Boolean = true) {
-        mediaPlayer.apply {
+        mediaPlayer?.apply {
             if (isPlaying) {
                 this.stop()
                 this.reset()
@@ -86,18 +45,22 @@ class AudioPlayer(
     }
 
     fun seekTo(timeInMillis: Long) {
-        mediaPlayer.apply {
-            if (isLoaded) mediaPlayer.seekTo(timeInMillis.toInt())
-        }
+        mediaPlayer?.apply { if (isLoaded) seekTo(timeInMillis.toInt()) }
     }
 
     fun release() {
         stop(release = false)
-        mediaPlayer.release()
+
+        progressCallbackHandler?.removeCallbacks(progressCallback)
+        progressCallbackHandler = null
+        progressCallback = null
+
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 
     private fun loadAudio(url: String, onAudioLoaded: () -> Unit) {
-        mediaPlayer.apply {
+        mediaPlayer?.apply {
             setOnErrorListener { mp, what, extra ->
                 Log.d(this::class.java.simpleName, "OnError - Error code: $what Extra code: $extra")
 
@@ -108,6 +71,52 @@ class AudioPlayer(
             reset()
             setDataSource(url)
             prepareAsync()
+        }
+    }
+
+    private fun initMediaPlayer() {
+        mediaPlayer = MediaPlayer().apply {
+            setOnPreparedListener {
+                // Start audio once loaded
+                start()
+
+                // Notify callback
+                onLoaded?.invoke(duration.toLong())
+
+                // Setup progress callback
+                initProgressCallback()
+                progressCallbackHandler?.postDelayed(progressCallback, TimeUnit.SECONDS.toMillis(1))
+
+                // Update flags
+                isLoaded = true
+            }
+
+            setOnCompletionListener {
+                // Notify callback
+                onCompleted?.invoke()
+
+                // Clear progress callback
+                progressCallbackHandler?.removeCallbacks(progressCallback)
+
+                // Update flags
+                isLoaded = false
+            }
+        }
+    }
+
+    private fun initProgressCallback() {
+        progressCallbackHandler = Handler()
+        progressCallback = Runnable {
+            val mediaPlayer = this.mediaPlayer
+            if (mediaPlayer != null && mediaPlayer.isPlaying) {
+                val progress = mediaPlayer.currentPosition.toLong()
+                if (progress != currentProgress) {
+                    onProgressChanged?.invoke(progress)
+                    currentProgress = progress
+                }
+
+                progressCallbackHandler?.postDelayed(progressCallback, TimeUnit.SECONDS.toMillis(1))
+            }
         }
     }
 }
